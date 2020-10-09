1fdc0f3c41f2553cb09a78a6737709342efaa39a
nhungnt <16520899@gm.uit.edu.vn>
2020-10-09 15:41:15 +0700

    initiation

---
+++ 6-Budgety/app.js
@@ -0,0 +1,22 @@
+var budgetController = (function(){
+
+})();
+
+var UIController = (function(){
+
+})();
+
+var controller = (function(budgetCtrl, UICtrl){
+
+    var ctrlAddItem = function(){
+        console.log("It's work");
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event){
+        if (event.keyCode === 13 || event.which === 13) {
+           ctrlAddItem();
+        }
+    });
+})(budgetController, UIController);
