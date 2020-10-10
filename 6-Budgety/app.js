8b666edfeaf08fe17f6a6753d48ab17b5817d25e
nhungnt <16520899@gm.uit.edu.vn>
2020-10-10 20:37:32 +0700

    data structure

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -1,5 +1,28 @@
 var budgetController = (function(){
 
+    var Expense = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = {
+        allItem: {
+            exp:[],
+            inc:[],
+        },
+        totalItem: {
+            exp: 0,
+            inc: 0,
+        }
+    }
+
 })();
 
 var UIController = (function(){
@@ -30,7 +53,7 @@ var UIController = (function(){
 var controller = (function(budgetCtrl, UICtrl){
 
     var setupEventListeners = function (){
-        
+
         var DOM = UICtrl.getDOMString();
 
         document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
@@ -46,7 +69,6 @@ var controller = (function(budgetCtrl, UICtrl){
     var ctrlAddItem = function(){
         // 1. Get the field input data from the
         var input = UICtrl.getInput();
-        console.log(input);
 
         // 2. Add the item to the budget
 
@@ -56,7 +78,16 @@ var controller = (function(budgetCtrl, UICtrl){
 
         // 5. Display the budget on the UI
 
+    };
+
+    return {
+        init: function(){
+            console.log('Application has started');
+            setupEventListeners();
+        }
     }
 
   
 })(budgetController, UIController);
+
+controller.init();
